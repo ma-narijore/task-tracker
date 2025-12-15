85b32beae49b03fdde9d82e5ee8833726791bb45
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-12-15 21:34:03 +0000

    create home page

--- task_tracker/app/views.py
+++ task_tracker/app/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+    return render(request, "app/index.html")
--- task_tracker/task_tracker/settings.py
+++ task_tracker/task_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "app",
 ]
 
 MIDDLEWARE = [
@@ -97,6 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = "app.Worker"
 
 
 # Internationalization
