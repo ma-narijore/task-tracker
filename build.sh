09f2d25c11cde7f8daf91844eca73834f4cc2510
NariJore <narijore@mail.com>
2026-07-21 21:24:35 +0100

    hjas

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python task_tracker/manage.py collectstatic --no-input
 
-python manage.py migrate
+python task_tracker/manage.py migrate
