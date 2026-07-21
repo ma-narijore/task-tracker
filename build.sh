65bba87a1354c6b75c4feeadd185cd31b8ca8de0
NariJore <narijore@mail.com>
2026-07-21 21:18:10 +0100

    fix deploy 2.0

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python task_tracker/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
-python task_tracker/manage.py migrate
+python manage.py migrate
